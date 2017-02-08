2e02006b69e3db8a2d2983f78fda97fadd6668d9
59455357291 <59455357291@NAC01058531.correiosnet.int>
2017-02-08 14:21:21 -0200

    10.6 - Criando entidades do pedido de venda

---
+++ VendaPedido/src/main/java/br/com/vendapedido/model/FormaPagamento.java
@@ -0,0 +1,7 @@
+package br.com.vendapedido.model;
+
+public enum FormaPagamento {
+	
+	DINHEIRO, CATAO_CREDITO, CARTAO_DEBITO, 
+	CHEQUE, BOLETO_BANCARIO, DEPOSITO_BANCARIO
+}
---
+++ VendaPedido/src/main/java/br/com/vendapedido/model/StatusPedido.java
@@ -0,0 +1,6 @@
+package br.com.vendapedido.model;
+
+public enum StatusPedido {
+
+	ORCAMENTO, EMITIDO, CANCELADO
+}
