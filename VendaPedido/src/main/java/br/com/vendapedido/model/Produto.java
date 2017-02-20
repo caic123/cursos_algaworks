eaaea4982cb2514d8700e34882539cb8d997f9c6
59455357291 <59455357291@NAC01058531.correiosnet.int>
2017-02-20 18:46:42 -0300

    11.7 - Customizando mensagens de validacao

--- VendaPedido/src/main/java/br/com/vendapedido/model/Produto.java
+++ VendaPedido/src/main/java/br/com/vendapedido/model/Produto.java
@@ -65,7 +65,7 @@ public class Produto implements Serializable {
 		this.sku = sku == null ? null : sku.toUpperCase();
 	}
 
-	@NotNull
+	@NotNull(message = "é obrigatório")
 	@Column(name="valor_unitario", nullable = false, precision = 10, scale = 2)
 	public BigDecimal getValorUnitario() {
 		return valorUnitario;
@@ -75,7 +75,7 @@ public class Produto implements Serializable {
 		this.valorUnitario = valorUnitario;
 	}
 	//valor nulo = NotNull
-	@NotNull @Min(0) @Max(9999)
+	@NotNull @Min(0) @Max(value = 9999, message = "tem um valor muito alto")
 	@Column(name="quantidade_estoque", nullable = false, length = 5)
 	public Integer getQuantidadeEstoque() {
 		return quantidadeEstoque;
