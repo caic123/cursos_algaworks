f13776c845d3adebdbc552f1609f65248f8006c1
59455357291 <59455357291@NAC01058531.correiosnet.int>
2017-03-13 18:00:39 -0300

    12.1 - Criando um produtor cdi para injetar entity manager

--- VendaPedido/src/main/java/br/com/vendapedido/controller/CadastroProdutoBean.java
+++ VendaPedido/src/main/java/br/com/vendapedido/controller/CadastroProdutoBean.java
@@ -1,11 +1,17 @@
 package br.com.vendapedido.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+import br.com.vendapedido.model.Categoria;
 import br.com.vendapedido.model.Produto;
 
 @Named
@@ -13,11 +19,21 @@ import br.com.vendapedido.model.Produto;
 public class CadastroProdutoBean implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-
+	
+	@Inject
+	private EntityManager manager;
+	
 	private Produto produto;
+	
+	private List<Categoria> categoriasRaizes;
 	
 	public CadastroProdutoBean(){
 		produto = new Produto();
+	}
+	
+	public void inicializar(){//VendaPU esta na persistence
+		//ResultList() -> Retorna uma lista
+		categoriasRaizes = manager.createQuery("from Categoria", Categoria.class).getResultList();
 	}
 	
 	public void salvar() {
@@ -29,6 +45,10 @@ public class CadastroProdutoBean implements Serializable{
 	
 	public void setProduto(Produto produto) {
 		this.produto = produto;
+	}
+
+	public List<Categoria> getCategoriaRaizes() {
+		return categoriasRaizes;
 	}
 
 }
