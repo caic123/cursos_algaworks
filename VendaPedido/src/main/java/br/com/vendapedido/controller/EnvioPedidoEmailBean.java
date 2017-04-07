1b6f8d648e0e5cac56184ebfbf4c62dfe3914274
cspassos <caic.souza@hotmail.com>
2017-04-07 14:12:14 -0300

    14.4 - Template de email com apache velocity

--- VendaPedido/src/main/java/br/com/vendapedido/controller/EnvioPedidoEmailBean.java
+++ VendaPedido/src/main/java/br/com/vendapedido/controller/EnvioPedidoEmailBean.java
@@ -1,13 +1,17 @@
 package br.com.vendapedido.controller;
 
 import java.io.Serializable;
+import java.util.Locale;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.apache.velocity.tools.generic.NumberTool;
+
 import br.com.vendapedido.controller.PedidoEdicao;
 import com.outjected.email.api.MailMessage;
+import com.outjected.email.impl.templating.velocity.VelocityTemplate;
 
 import br.com.vendapedido.model.Pedido;
 import br.com.vendapedido.util.jsf.FacesUtil;
@@ -31,7 +35,10 @@ public class EnvioPedidoEmailBean implements Serializable{
 		
 		message.to("cspassos.vendas1@gmail.com")
 			.subject("Pedido " + this.pedido.getId())
-			.bodyHtml("<strong>Valor total:</strong> " + this.pedido.getValorTotal())
+			.bodyHtml(new VelocityTemplate(getClass().getResourceAsStream("/emails/pedido.template")))// vai pegar o pedido.template para jogar no email
+			.put("pedido", this.pedido)
+			.put("numberTool", new NumberTool())
+			.put("locale", new Locale("pt", "BR"))
 			.send();
 		
 		FacesUtil.addInfoMessage("Pedido enviado por e-mail com sucesso!");
